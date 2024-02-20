e56aa6a9a65edd587ccde5818da89f6ac5ae4f9b
michaeland974 <micanderson29@gmail.com>
2024-02-20 14:02:43 -0600

    about-me content transition added

--- src/listeners.ts
+++ src/listeners.ts
@@ -1,1 +1,17 @@
-console.log("Hello wolrd");
+const fade = {
+  selector: `#about-me *[will-transition="true"][fade-status="false"]`,
+  getTargets: function(): NodeListOf<Element>{
+    return document.querySelectorAll(this.selector)
+  }
+};
+
+const aboutMeBio: Element | null = document.querySelector("#bio");
+
+aboutMeBio?.addEventListener("click", function(this: Element | null){
+  const targets = fade.getTargets();
+  targets.forEach((el) => {
+    el.setAttribute("fade-status", "true");
+  });
+
+  this?.setAttribute("translate-status", "true");
+});
